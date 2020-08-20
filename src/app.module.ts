28694d5c110cd8fce67a56adb2dc31e001aa01c3
Choco <jonathan01.07.95@gmail.com>
2020-08-20 08:11:21 -0500

    prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatosUsuarioController } from './datos-usuario/datos-usuario.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, DatosUsuarioController],
   providers: [AppService],
 })
 export class AppModule {}
